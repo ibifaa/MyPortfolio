6370e2e1c920015f64c33755d1a8f11c84d808e3
devibifaa <ib2link@yahoo.com>
2023-11-17 16:58:20 +0100

    mobile update

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let d = new Date();
 let year = d.getFullYear()
 date.innerHTML=year;
 console.log(year)
+
+let links = document.querySelector(".links");
+let button = document.querySelector(".icon-hamburger");
+
+button.addEventListener("click" , ()=>{
+    links.classList.toggle("showLinks")
+})
