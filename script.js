3f375e30955b7eebb0cd0c864ef1d5c03670ba21
devibifaa <ib2link@yahoo.com>
2023-11-22 05:03:28 +0100

    updated the nav-section by adding event listener and making the nav item to spread across the page

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ console.log(year)
 
 let links = document.querySelector(".links");
 let button = document.querySelector(".icon-hamburger");
+let closeBtn = document.querySelector(".close");
 
 button.addEventListener("click" , ()=>{
     links.classList.toggle("showLinks")
+})
+
+closeBtn.addEventListener("click" , ()=>{
+    links.classList.toggle("closeLinks")
 })
