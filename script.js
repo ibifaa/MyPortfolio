452df890d2169a81bad11d12f79ca902fc974d9d
devibifaa <ib2link@yahoo.com>
2023-11-27 17:56:12 +0100

    update new design

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let date = document.querySelector(".date");
+let navbar = document.querySelector(".navbar");
+let navLinks = document.querySelector(".links ");
+let logo = document.querySelector(".logo ");
+// let name = document.querySelectorAll(".name")
 
 let d = new Date();
 let year = d.getFullYear()
@@ -16,3 +20,28 @@ button.addEventListener("click" , ()=>{
 closeBtn.addEventListener("click" , ()=>{
     links.classList.toggle("closeLinks")
 })
+
+
+window.addEventListener("scroll" ,()=>{
+    if(window.scrollY>0){
+        navbar.style.backgroundColor = "goldenrod";
+        navLinks.style.color = "white";
+        logo.style.backgroundColor ="black"
+        logo.style.color ="goldenrod"
+    }
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    const names = document.querySelectorAll(".name")
+    let index = 0;
+
+    function displayNextName(){
+        if(index <names.length){
+            names[index].style.opacity = 1;
+            index++;
+            setInterval(displayNextName, 2000);
+        }
+    }
+
+    displayNextName();
+});
